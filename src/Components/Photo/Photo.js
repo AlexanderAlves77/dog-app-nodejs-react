df5ba85d7e1206deceb860021815ea60986233b0
AlexanderAlves77 <vendas77.rj@gmail.com>
2021-07-28 10:37:09 -0300

    Atualizando os arquivos na pasta Feed

---
+++ src/Components/Photo/Photo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PhotoContent from './PhotoContent'
+
+const Photo = () => {
+  return (
+    <div>
+      <PhotoContent />
+    </div>
+  )
+}
+
+export default Photo
